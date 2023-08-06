38c7509d6f325d42e47d80126f9bff76abccf295
James Aman <jim.a.aman@gmail.com>
2023-08-05 21:18:11 -0500

    intro to Effect and IO

---
+++ src/main/scala/com/rockthejvm/part2effects/Effects.scala
@@ -0,0 +1,77 @@
+package com.rockthejvm.part2effects
+
+import scala.concurrent.Future
+
+object Effects {
+
+  // pure functional programming
+  // substitution (referential transparency)
+  //   is needed in order for a program to be a single expression to be evaluate
+  def combine(a: Int, b: Int): Int = a + b
+  val five = combine(2, 3)
+  val five_v2 = 2 + 3
+  val five_v3 = 5
+
+  // referential transparency = can replace an expression with its value
+  //     as many times as we want without changing behavior
+
+  // example: print to the console
+  val printSomething: Unit = println("Cats effect")
+  val printSomething_v2: Unit = () // not the same as program with printSomething
+
+  // example: change a variable
+  var anInt = 0
+  val changingVar: Unit = anInt += 1
+  val changingVar_v2: Unit = () // again lose referential transparency since this isn't the same
+
+  // side-effects are inevitable for any useful program
+  // effect is the wrapper of a side-effect into the application domain
+  // Desires
+  // - type signature describes the kind of calculation that will be performed
+  // - type signature describes the VALUE that will be calculated
+  // - when side-effects are needed, effect construction is separate from effect execution
+
+  // example: Option
+  // - describes a possibly absent value
+  // - computes a value of type A, if it exists
+  // - side effects are not needed
+  // --- according to the above definition, Option is an effect type
+  val anOption: Option[Int] = Option(42)
+
+  /**
+   * what about Future?
+   * - describes an asynchronous computation that will perform in the future
+   * - computes a value of type A, if it is successful
+   * - side effect is required (allocating/scheduling a thread) -> execution is NOT separate from construction
+   * --- Future is not a good effect type
+   */
+  import scala.concurrent.ExecutionContext.Implicits.global
+  val aFuture: Future[Int] = Future(42)
+
+  /**
+   * MyIO data type from the Monads lesson
+   * - describes a computation that might produce side-effects
+   * - calculates a value of type A, if it is successful
+   * - side-effects are required for the evaluation of () => A
+   * --- So MyIO is an effect data type since the construction is separate from the side-effect action
+   */
+
+  case class MyIO[A](unsafeRun: () => A) {
+    def map[B](f: A => B): MyIO[B] =
+      MyIO(() => f(unsafeRun()))
+
+    def flatMap[B](f: A => MyIO[B]): MyIO[B] =
+      MyIO(() => f(unsafeRun())).unsafeRun()
+  }
+
+  val anIO: MyIO[Int] = MyIO(() => {
+    println("I'm writing something")
+    42
+  })
+
+  def main(args: Array[String]): Unit = {
+
+    //anIO.unsafeRun()
+  }
+
+}
