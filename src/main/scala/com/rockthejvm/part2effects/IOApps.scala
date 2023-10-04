b8ec2bfaca8866e49b6ffeef2e5bfef973796a07
James Aman <jim.a.aman@gmail.com>
2023-10-04 01:13:59 -0500

    IO Apps

--- src/main/scala/com/rockthejvm/part2effects/IOApps.scala
+++ src/main/scala/com/rockthejvm/part2effects/IOApps.scala
@@ -1,5 +1,32 @@
 package com.rockthejvm.part2effects
 
-object IOApps {
+import cats.effect.{ExitCode, IO, IOApp}
 
+import scala.io.StdIn
+
+object IOApps {
+  val program: IO[Unit] = for {
+    line <- IO(StdIn.readLine())
+    _ <- IO(println(s"You've just written: $line"))
+  } yield ()
+}
+
+object TestApp {
+  import IOApps._
+
+  def main(args: Array[String]): Unit = {
+    import cats.effect.unsafe.implicits.global
+    program.unsafeRunSync()
+  }
+}
+
+object MyFirstIOApp extends IOApp {
+  import IOApps._
+  override def run(args: List[String]): IO[ExitCode] = program.as(ExitCode.Success)
+}
+
+object MySimpleApp extends IOApp.Simple {
+  import IOApps._
+
+  override def run = program
 }
